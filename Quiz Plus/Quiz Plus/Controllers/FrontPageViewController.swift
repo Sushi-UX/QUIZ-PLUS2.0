1d514f84201dfd22f0eac55c0a0fde50cf248303
Sushi-UX <thechoygroup@gmail.com>
2019-07-11 01:25:41 -0400

    Created layout of start page
    Created layout of start page

--- Quiz Plus/Quiz Plus/Controllers/FrontPageViewController.swift
+++ Quiz Plus/Quiz Plus/Controllers/FrontPageViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class FrontPageViewController: UIViewController {
 
+    @IBAction func StartPress(_ sender: Any) {
+        performSegue(withIdentifier: "startToQuiz", sender: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
 
